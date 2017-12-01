0c3640d36e541aceff921272ba033cf5a10a034a
martsberger <bmarts@procuredhealth.com>
2017-11-30 21:10:46 -0600

    Add travis.yml and test settings files

---
+++ sql_util/tests/test_postgres_sttings.py
@@ -0,0 +1,21 @@
+BACKEND = 'postgres'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'utils',
+    }
+}
+
+INSTALLED_APPS = (
+    'sql_util.tests',
+)
+
+SITE_ID = 1,
+
+SECRET_KEY = 'secret'
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+)
