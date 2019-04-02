413399de4b8f5defb398a8452b2398edccdb223c
martsberger <bmarts@lumere.com>
2019-04-01 23:50:30 -0400

    Add setup file

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+
+def read_file(name):
+    with open(name) as fd:
+        return fd.read()
+
+
+setup(
+    name='django-sql-utils',
+    version='0.1.0',
+    description='Improved API for aggregating using Subquery',
+    long_description=read_file('README.rst'),
+    url='https://github.com/martsberger/django-sql-utils',
+    download_url='https://github.com/martsberger/django-sql-utils/archive/0.1.0.tar.gz',
+    author='Brad Martsberger',
+    author_email='bmarts@lumere.com',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ],
+    packages=find_packages(),
+    install_requires=['django>=1.11']
+)
