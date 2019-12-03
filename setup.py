24f2330b05ab01b38e431fe643d744df49591ae4
martsberger <bmarts@lumere.com>
2019-12-03 10:20:02 -0500

    Missing a comma in setup classifiers.

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7'
+        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8'
     ],
     packages=find_packages(),
