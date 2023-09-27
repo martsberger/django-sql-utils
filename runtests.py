90379c648142eb75c7b976a2a444be297a8008af
martsberger <bradley.marts@gmail.com>
2023-09-27 11:28:01 -0400

    clean up the test runner

--- runtests.py
+++ runtests.py
@@ -5,25 +5,13 @@ from optparse import OptionParser
 
 def parse_args():
     parser = OptionParser()
-    parser.add_option('-s', '--settings', help='Define settings.')
-    parser.add_option('-t', '--unittest', help='Define which test to run. Default all.')
+    parser.add_option('-s', '--settings', help='Define settings.', default='sql_util.tests.test_sqlite_settings')
     parser.add_option('-v', '--verbosity', help='Set the verbosity level.', default=1, type=int)
-    options, args = parser.parse_args()
-
-    if not options.settings:
-        parser.print_help()
-        sys.exit(1)
-
-    if not options.unittest:
-        options.unittest = []
-    else:
-        options.unittest = [options.unittest]
-
-    return options
+    return parser.parse_args()
 
 
 if __name__ == '__main__':
-    options = parse_args()
+    options, tests = parse_args()
     os.environ['DJANGO_SETTINGS_MODULE'] = options.settings
 
     # Local imports because DJANGO_SETTINGS_MODULE needs to be set first
@@ -36,4 +24,4 @@ if __name__ == '__main__':
 
     TestRunner = get_runner(settings)
     runner = TestRunner(verbosity=options.verbosity, interactive=True, failfast=False)
-    sys.exit(runner.run_tests(options.unittest))
+    sys.exit(runner.run_tests(tests))
